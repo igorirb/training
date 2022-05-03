958e58a9a8c68df9c2a9da257720905334e0562e
Igor Barroso <igor.irb@gmail.com>
2022-05-02 22:00:55 -0300

    feat(sorting) merge sorted arrays

---
+++ trees/merge-sorted-arrays.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        while (k >= 0 && (i >= 0 || j >= 0)) {
+            if (i >= 0 && (j < 0 || nums1[i] >= nums2[j])) {
+                nums1[k] = nums1[i];
+                i -= 1;
+            } else if (j >= 0 && (i < 0 || nums1[i] <= nums2[j])) {
+                nums1[k] = nums2[j];
+                j -= 1;
+            }
+            k -= 1;
+        }
+    }
+};
