d529da640de94291415ae66659b4c3361ba46df8
Igor Barroso <igor.irb@gmail.com>
2022-05-16 21:46:35 -0300

    feat(math) solution not using loop nor recursion

---
+++ math/power-of-three-v3.cpp
@@ -0,0 +1,9 @@
+// Solution not using loop nor recursion
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if (n <= 0) return false;
+        int x = log2(n) / log2(3);
+        return pow(3, x) == n;
+    }
+};
