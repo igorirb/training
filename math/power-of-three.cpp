b2c26ac0f09f2e265fca959f046b559cf0f70146
Igor Barroso <igor.irb@gmail.com>
2022-05-16 21:40:40 -0300

    feat(math) solution using loop to find out if n is power of three

---
+++ math/power-of-three.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if (n < 0) return false;
+        bool result = false;
+        for (long test = 1; test <= n; test *= 3) {
+            if (test == n) {
+                result = true;
+            }
+        }
+        return result;
+    }
+};
